8026449eb9d4ca417cacfb644c23a8091db04d0b
Fabiano Parreira <fabiano.parreira@soreto.com>
2023-01-23 23:37:24 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from "next";
 import PostPage from "../components/PostPage/PostPage";
 import { post } from "../mock/post";
-import { getUsers } from "../mock/users";
+// import { getUsers } from "../mock/users";
 import { IPostData } from "../model/post";
 
 const Post: React.FC<IPostData> = ({postData}) => {
@@ -14,8 +14,8 @@ const Post: React.FC<IPostData> = ({postData}) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = post; // Here the services will be called
-  const users = await getUsers();
-  console.log(users)
+  // const users = await getUsers();
+  // console.log(users)
   return {
     props: {
       postData: posts
